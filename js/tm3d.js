4062a2bbc95bc584616a5c74c9025b8e6b0816dc
duyalei <3209742516@qq.com>
2016-04-29 16:36:22 +0800

    添加到github

---
+++ js/tm3d.js
@@ -0,0 +1,19 @@
+(function ($) {
+	$.fn.extend({
+		pic3d : function(opts){
+
+		}
+	})
+
+	var opts_default = {
+		'ele' : $('#car'),
+		'imgLength' : 51,
+		'autoplay' : true,
+		'auto' : {
+			imgtime : 300,
+			dir : 'right',
+			delaytime : 2000
+		}
+	}
+    
+})(window.jQuery);
